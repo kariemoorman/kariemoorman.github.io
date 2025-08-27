f8326bb54e05fc92c0b80c29ca43f9f955716893
karie <25291562+kariemoorman@users.noreply.github.com>
2025-08-26 19:28:53 -0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
-const imageContainers = document.querySelectorAll('.carousel-images .image');
-const mainImage = document.querySelector('.main-image');
-const imageTitle = document.querySelector('.image-title');
-let currentIndex = 0;
+// const imageContainers = document.querySelectorAll('.carousel-images .image');
+// const mainImage = document.querySelector('.main-image');
+// const imageTitle = document.querySelector('.image-title');
+// let currentIndex = 0;
 
 // imageContainers.forEach((container) => {
 //   container.addEventListener('click', () => {
@@ -11,20 +11,20 @@ let currentIndex = 0;
 //   });
 // });
 
-function updateCarousel(index) {
-  const selectedImage = imageContainers[index].querySelector('img');
-  mainImage.src = selectedImage.src;
-  imageTitle.textContent = selectedImage.alt;
-}
+// function updateCarousel(index) {
+//   const selectedImage = imageContainers[index].querySelector('img');
+//   mainImage.src = selectedImage.src;
+//   imageTitle.textContent = selectedImage.alt;
+// }
 
-mainImage.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % imageContainers.length;
-  updateCarousel(currentIndex);
-});
+// mainImage.addEventListener('click', () => {
+//   currentIndex = (currentIndex + 1) % imageContainers.length;
+//   updateCarousel(currentIndex);
+// });
 
-imageContainers.forEach((container, index) => {
-  container.addEventListener('click', () => {
-    updateCarousel(index);
-  });
-});
+// imageContainers.forEach((container, index) => {
+//   container.addEventListener('click', () => {
+//     updateCarousel(index);
+//   });
+// });
   
